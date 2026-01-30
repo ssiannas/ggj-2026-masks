aee8206863f6d95e3d4d3103afc5504df37eb498
George Tzoupis <giwrgos0gt15@gmail.com>
2026-01-30 23:27:12 +0200

    Added player attack button triggering

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5.0f;
-
     private Rigidbody rb;
+
+    // Use to assign a callback for when attack is triggered
+    public UnityAction OnAttackTriggered;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        OnAttackTriggered += HandleAttack;
     }
 
     // Update is called once per frame
@@ -24,5 +28,23 @@ public class PlayerController : MonoBehaviour
         var direction = value.Get<Vector2>();
 
         rb.linearVelocity = moveSpeed * new Vector3(direction.x, 0, direction.y);
+    }
+
+    void OnAttack(InputValue value)
+    {
+        var attackButton = value.Get<float>();
+        
+        // Check if attack is triggered. Add additional conditions here.
+        var shouldAttack = attackButton > 0.5f;
+
+        if (shouldAttack)
+        {
+            OnAttackTriggered.Invoke();
+        }
+    }
+
+    void HandleAttack()
+    {
+        Debug.Log("Attacking!");
     }
 }
