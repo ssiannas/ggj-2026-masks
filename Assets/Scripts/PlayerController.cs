a3f3b7311160759d1f94ab44fa827b6b0814aabc
George Tzoupis <giwrgos0gt15@gmail.com>
2026-01-31 02:57:58 +0200

    Added Player UI component

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,109 +1,119 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
+[Serializable]
+public class FloatEvent : UnityEvent<float>
+{
+}
+
 public class PlayerController : MonoBehaviour
 {
+    // Move
     [SerializeField] private float moveSpeed = 5.0f;
-    private Rigidbody rb;
-    private Vector2 moveDirection;
-
-    // Dash state
-    private bool isDashing = false;
-    private float dashStartTime;
 
     // Attacking
-    public UnityAction OnAttackTriggered;
+    public UnityEvent OnAttackTriggered;
 
     // Health
     [SerializeField] private float maxHealth = 100.0f;
+    public UnityEvent OnPlayerDeath;
+    public FloatEvent OnHealthUpdated;
+
+    // Dashing
+    [SerializeField] private float dashSpeed = 15.0f;
+    [SerializeField] private float dashDurationMs = 20.0f;
+    public UnityEvent OnDashTriggered;
     private float _health;
-    public UnityAction OnPlayerDeath;
+    private float dashStartTime;
+
+    // Dash state
+    public bool isDashing;
+    private Vector2 moveDirection;
+    private Rigidbody rb;
+
+    public bool isAlive => Health > 0;
+
     public float Health
     {
         get => _health;
         set
         {
             _health = value;
-            if (_health < 0)
-            {
-                OnPlayerDeath.Invoke();
-            }
+            OnHealthUpdated.Invoke(_health / maxHealth);
+            if (_health < 0) OnPlayerDeath.Invoke();
         }
     }
-    
-    // Dashing
-    [SerializeField] private float dashSpeed = 15.0f;
-    [SerializeField] private float dashDurationMs = 20.0f;
-    public UnityAction OnDashTriggered;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        OnAttackTriggered += HandleAttack;
+        OnAttackTriggered.AddListener(HandleAttack);
         _health = maxHealth;
-        OnPlayerDeath += HandlePlayerDeath;
-        OnDashTriggered += HandleDash;
+        OnPlayerDeath.AddListener(HandlePlayerDeath);
+        OnDashTriggered.AddListener(HandleDash);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Check if dash duration has expired
-        if (isDashing && (Time.time - dashStartTime) * 1000 >= dashDurationMs)
+        // Dead => no movement
+        if (!isAlive)
         {
-            isDashing = false;
-        }
+            rb.linearVelocity = Vector3.zero;
+            return;
+        };
+
+        // Check if dash duration has expired
+        if (isDashing && (Time.time - dashStartTime) * 1000 >= dashDurationMs) isDashing = false;
 
         // Apply movement
         if (isDashing)
-        {
             rb.linearVelocity = dashSpeed * new Vector3(moveDirection.x, 0, moveDirection.y);
-        }
         else
-        {
             rb.linearVelocity = moveSpeed * new Vector3(moveDirection.x, 0, moveDirection.y);
-        }
     }
 
-    void OnMove(InputValue value)
+    private void OnMove(InputValue value)
     {
         moveDirection = value.Get<Vector2>();
     }
 
-    void OnAttack(InputValue value)
+    private void OnAttack(InputValue value)
     {
         var attackButton = value.Get<float>();
-        
+
         // Check if attack is triggered. Add additional conditions here.
         var shouldAttack = attackButton > 0.5f;
 
-        if (shouldAttack)
-        {
-            OnAttackTriggered.Invoke();
-        }
+        if (shouldAttack) OnAttackTriggered.Invoke();
     }
 
-    void OnDash(InputValue value)
+    private void OnDash(InputValue value)
     {
         var dashButton = value.Get<float>();
         var shouldDash = dashButton > 0.5f;
 
-        if (shouldDash)
-        {
-            OnDashTriggered.Invoke();
-        }
+        if (shouldDash) OnDashTriggered.Invoke();
     }
 
-    void HandleAttack()
+    private void OnCrouch(InputValue value)
+    {
+        var crouchButton = value.Get<float>();
+        if (crouchButton > 0.5f) ApplyDamage(0.2f * maxHealth);
+    }
+
+    private void HandleAttack()
     {
         Debug.Log("Attacking!");
     }
 
-    void HandlePlayerDeath()
+    private void HandlePlayerDeath()
     {
         Debug.Log("Player Died!");
+        isDashing = false;
     }
 
     public void ApplyDamage(float damage)
@@ -111,7 +121,7 @@ public class PlayerController : MonoBehaviour
         Health -= damage;
     }
 
-    void HandleDash()
+    private void HandleDash()
     {
         isDashing = true;
         dashStartTime = Time.time;
---
+++ Assets/Scripts/PlayerUIController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerUIController : MonoBehaviour
+{
+    [SerializeField] private Image healthBarImage;
+    [SerializeField] private float animationSpeed = 10.0f;
+    private float _animatedHealthPct;
+
+    private float _currentHealthPct;
+
+    private float _originalWidth;
+
+    private void Awake()
+    {
+        _originalWidth = healthBarImage.rectTransform.sizeDelta.x;
+        _currentHealthPct = 1f;
+        _animatedHealthPct = 1f;
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(_animatedHealthPct - _currentHealthPct) > 0.001f)
+        {
+            _animatedHealthPct = Mathf.Lerp(_animatedHealthPct, _currentHealthPct, animationSpeed * Time.deltaTime);
+            ApplyHealthBar(_animatedHealthPct);
+        }
+    }
+
+    public void SetHealthPercentage(float pct)
+    {
+        _currentHealthPct = Mathf.Clamp01(pct);
+    }
+
+    private void ApplyHealthBar(float pct)
+    {
+        var sizeDelta = healthBarImage.rectTransform.sizeDelta;
+        sizeDelta.x = _originalWidth * pct;
+        healthBarImage.rectTransform.sizeDelta = sizeDelta;
+    }
+}
